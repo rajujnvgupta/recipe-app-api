5e5cf7d9533b94fb1db1321b0dbcae58a2baae1f
rajujnvgupta <cserajugupta2021@gmail.com>
2023-02-09 09:28:43 +0530

    adding django test and database configuration

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+    calculator making 
+
+"""
+
+def add(x, y):
+    """ add x and y and return results. """
+    return x + y
+def substract(x, y):
+    """ substract x from y and rerutn result."""
+    return y - x 
---
+++ app/app/tests.py
@@ -0,0 +1,25 @@
+"""
+    sample tests
+
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+
+class CalcTests(SimpleTestCase):
+    """ test the calc module. """
+
+    def test_add_numbers(self):
+        """ test adding number together """
+
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+    def test_substract_number(self):
+        """ test substracting number. """
+        res = calc.substract(10, 15)
+        self.assertEqual(res, 5)
+
+
